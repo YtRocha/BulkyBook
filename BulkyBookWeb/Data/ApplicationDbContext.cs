7872eec5f4e107f9f5c5c38c67a319bf1b3398bc
ytallo <ytallo4gamer@gmail.com>
2024-02-29 16:36:11 -0300

    Adicionar arquivos de projeto.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
