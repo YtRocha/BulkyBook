b20e67025a623b3441f280dd317cca5ebbb3347c
ytallo <ytallo4gamer@gmail.com>
2024-03-01 19:21:05 -0300

    Criação view e ctr de category

---
+++ BulkyBookWeb/Controllers/CategoryyController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryyController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryyController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+
+            return View(objCategoryList);
+        }
+    }
+}
