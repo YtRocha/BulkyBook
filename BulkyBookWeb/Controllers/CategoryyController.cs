d35a115b354d23c9441a9c57815c90ae4b2e4cec
ytallo <ytallo4gamer@gmail.com>
2024-03-25 20:33:11 -0300

    view and post of create category created without validations.

--- BulkyBookWeb/Controllers/CategoryyController.cs
+++ BulkyBookWeb/Controllers/CategoryyController.cs
@@ -21,11 +21,25 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+        //GET
         public IActionResult Create()
         {
-            
+
 
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
